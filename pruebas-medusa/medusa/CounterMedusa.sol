22f41dc36e91184409fc612eae0593c57e8ba946
root <root@LAPTOP-EKDRPG1D.>
2026-08-03 11:32:42 +0200

    init commit

---
+++ pruebas-medusa/medusa/CounterMedusa.sol
@@ -0,0 +1,23 @@
+pragma solidity 0.8.13;
+
+import "../contracts/Counter.sol";
+
+contract CounterMedusaTest is Counter {
+    function testSetNumber(uint16 num) public {
+        setNumber(num);
+        assert(number == num);
+    }
+
+    function testIncrement(uint16 num, uint16 times) public {
+        setNumber(num);
+        increment(times);
+        assert(number == num + times);
+    }
+
+    function testDouble(uint16 num, uint16 a, uint16 b) public {
+        uint256 total = uint256(a) * uint256(b);
+        setNumber(num);
+        double(a, b);
+        assert(number == num + uint16(total));
+    }
+}
